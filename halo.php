760643da773cc2ba9dc4e9bfe237642cf62044ee
Duevano Fairuz Pandya <114162555+duevanofairuz@users.noreply.github.com>
2023-10-30 13:25:26 +0700

    Add files via upload

---
+++ halo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halo Dunia Code Igniter</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Halo Dunia dari Code Igniter</h1><br>";
+        echo "<h2>Hari ini tanggal " . date("Y-m-d") . "<h2><br>";
+    ?>
+</body>
+</html>
